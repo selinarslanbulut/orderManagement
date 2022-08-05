2380f8587d29c0807916cc65239206a376d913b0
selinarslanbulut <arslanbulutselin@outlook.com>
2022-08-05 16:16:55 +0300

    ngxs / api

---
+++ libs/enums/http.ts
@@ -0,0 +1,14 @@
+export namespace eHTTP {
+  export enum Observe {
+    Body = 'body',
+    Events = 'events',
+    Response = 'response',
+  }
+
+  export enum ResponseType {
+    ArrayBuffer = 'arraybuffer',
+    Blob = 'blob',
+    JSON = 'json',
+    Text = 'text',
+  }
+}
